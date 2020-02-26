16e14d60b7875300022e6d9221a6821712b7e0eb
Tamás <codesigner.hu@gmail.com>
2020-02-27 00:57:09 +0100

    developing logout

--- src/view/Logout/Logout.js
+++ src/view/Logout/Logout.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
+import girhubPic from "../../assets/github.png";
+import "./Logout.css";
 
 class Logout extends Component {
   state = {};
 
   render() {
-    return <div>Logout</div>;
+    return (
+      <div className="lg-container">
+        <div className="label-continer">
+          <p> Successfull logout!</p>
+          <p> Thank you for choosing our service!</p>
+        </div>
+        <div className="image-container">
+          <img src={girhubPic} alt="Github Picture" />
+        </div>
+      </div>
+    );
   }
 }
 
