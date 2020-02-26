c7dd03162e4aecd093bde94504c4b92419295a29
Tamás <codesigner.hu@gmail.com>
2020-02-26 23:03:40 +0100

    improving signup and login views

--- src/view/Login/Login.js
+++ src/view/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Button from "../../components/Button/Button";
+import Form from "../../components/Form/Form";
 
 import "./Login.css";
 
@@ -23,6 +23,7 @@ class Login extends Component {
         [input]: targetValue
       }
     });
+    console.log("state", this.state);
   };
 
   logIn = () => {
@@ -46,53 +47,16 @@ class Login extends Component {
   };
 
   render() {
-    console.log("state", this.state);
     let { emailWarning, passwordWarning } = this.state;
     return (
       <div className="container">
         <div className="card-container">
-          <div className="card">
-            <div>
-              <h2>Github Search</h2>
-            </div>
-            <div>
-              <form className="input-container" action="">
-                <input
-                  type="text"
-                  placeholder="Email"
-                  onChange={this.handleChange("email")}
-                />
-                {emailWarning === true ? (
-                  <span className="warningMsg">
-                    Kérjük add meg az email címed!
-                  </span>
-                ) : null}
-                <input
-                  type="text"
-                  placeholder="Password"
-                  onChange={this.handleChange("password")}
-                />
-                {passwordWarning === true ? (
-                  <span className="warningMsg">
-                    Kérjük add meg a jelszavad!
-                  </span>
-                ) : null}
-              </form>
-              <Button clicked={this.logIn}> Log in </Button>
-            </div>
-            <div className="text-container">
-              <span>
-                Please sign up or sign in to use the GitHub Search App.
-              </span>
-              <span>
-                Already have an account? Click this{" "}
-                <span>
-                  <a href="">link</a>
-                </span>{" "}
-                for sign in.
-              </span>
-            </div>
-          </div>
+          <Form
+            handleChange={this.handleChange}
+            logIn={this.logIn}
+            emailWarning={emailWarning}
+            passwordWarning={passwordWarning}
+          />
         </div>
       </div>
     );
